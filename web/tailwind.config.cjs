c3f81cf3a7123d186ad35836348f72637c201d9e
RicardoFerraz <sourvivens@gmail.com>
2022-09-13 16:10:42 -0300

    Layout landing page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,11 +5,14 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 43.94%, #E1D55D 34.57%)',
-
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
